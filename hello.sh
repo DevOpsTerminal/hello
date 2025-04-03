c4c9e3cd0718aee3889f1150958100ad3b14e53f
Tom Softreck <info@softreck.dev>
2025-04-03 12:47:29 +0200

    Release version 0.1.9

--- hello.sh
+++ hello.sh
@@ -1199,12 +1199,10 @@ main() {
     # Inicjalizacja zmiennych śledzenia zmian
     TRACK_CHANGES=false
     SINCE_DATE="$DEFAULT_SINCE_DATE"
-    RUN_GUI=true  # Domyślnie uruchamiamy interfejs użytkownika
 
     # Parsowanie argumentów linii poleceń
     if [ "$#" -gt 0 ]; then
-        RUN_GUI=false  # Jeśli są argumenty, nie uruchamiamy GUI
-
+        # Tryb linii poleceń
         for arg in "$@"; do
             case $arg in
                 --track-changes=*)
@@ -1272,24 +1270,22 @@ main() {
         echo -e "${RED}Błąd: Nie podano wymaganych argumentów${RESET}"
         display_help
         exit 1
-    fi
-
-    # Sprawdź, czy skrypt jest uruchomiony jako root - tylko w trybie interaktywnym
-    if [ "$RUN_GUI" = true ] && [ "$(id -u)" != "0" ]; then
-        echo -e "${RED}Uwaga: Skrypt nie jest uruchomiony jako root. Niektóre informacje mogą być niedostępne.${RESET}"
-        echo -e "${YELLOW}Zalecane ponowne uruchomienie z sudo:${RESET} sudo $0\n"
-        read -r -p "Kontynuować mimo to? (t/n): " -n 1 REPLY
-        echo
-        if [[ ! $REPLY =~ ^[Tt]$ ]]; then
-            exit 1
-        fi
-    fi
-
-    # Interfejs użytkownika (tylko gdy nie ma argumentów i RUN_GUI=true)
-    if [ "$RUN_GUI" = true ]; then
+    else
+        # Tryb interaktywny
         echo -e "${BLUE}===== Linux Software Finder =====${RESET}"
         echo -e "${YELLOW}Ten skrypt identyfikuje zainstalowane oprogramowanie i usługi${RESET}"
         echo -e "${YELLOW}oraz śledzi zmiany w systemie od określonej daty${RESET}"
+
+        # Sprawdź, czy skrypt jest uruchomiony jako root
+        if [ "$(id -u)" != "0" ]; then
+            echo -e "${RED}Uwaga: Skrypt nie jest uruchomiony jako root. Niektóre informacje mogą być niedostępne.${RESET}"
+            echo -e "${YELLOW}Zalecane ponowne uruchomienie z sudo:${RESET} sudo $0\n"
+            read -r -p "Kontynuować mimo to? (t/n): " -n 1 REPLY
+            echo
+            if [[ ! $REPLY =~ ^[Tt]$ ]]; then
+                exit 1
+            fi
+        fi
 
         # Menu wyboru
         while true; do
@@ -1301,135 +1297,115 @@ main() {
             fi
             echo
 
-            PS3="Wybierz opcję: "
-            options=("Wyświetl wszystkie informacje"
-                    "Informacje o systemie"
-                    "Zainstalowane pakiety"
-                    "Uruchomione usługi"
-                    "Otwarte porty"
-                    "Programy startowe"
-                    "Ostatnio zainstalowane pakiety"
-                    "Skonfigurowane repozytoria"
-                    "Ustaw datę śledzenia zmian"
-                    "Zmiany w plikach konfiguracyjnych"
-                    "Zmiany w użytkownikach/grupach"
-                    "Zmiany w plikach binarnych"
-                    "Zmiany w zaplanowanych zadaniach"
-                    "Zmiany w konfiguracji sieci"
-                    "Zmiany w kluczowych plikach systemowych"
-                    "Raport zmian miesięcznych (ostatnie 12 miesięcy)"
-                    "Statystyki miesięczne (ostatnie 12 miesięcy)"
-                    "Wizualizacja zmian miesięcznych"
-                    "Zapisz wszystko do pliku"
-                    "Pomoc"
-                    "Wyjście")
+            echo " 1) Wyświetl wszystkie informacje"
+            echo " 2) Informacje o systemie"
+            echo " 3) Zainstalowane pakiety"
+            echo " 4) Uruchomione usługi"
+            echo " 5) Otwarte porty"
+            echo " 6) Programy startowe"
+            echo " 7) Ostatnio zainstalowane pakiety"
+            echo " 8) Skonfigurowane repozytoria"
+            echo " 9) Ustaw datę śledzenia zmian"
+            echo "10) Zmiany w plikach konfiguracyjnych"
+            echo "11) Zmiany w użytkownikach/grupach"
+            echo "12) Zmiany w plikach binarnych"
+            echo "13) Zmiany w zaplanowanych zadaniach"
+            echo "14) Zmiany w konfiguracji sieci"
+            echo "15) Zmiany w kluczowych plikach systemowych"
+            echo "16) Raport zmian miesięcznych (ostatnie 12 miesięcy)"
+            echo "17) Statystyki miesięczne (ostatnie 12 miesięcy)"
+            echo "18) Wizualizacja zmian miesięcznych"
+            echo "19) Zapisz wszystko do pliku"
+            echo "20) Pomoc"
+            echo "21) Wyjście"
+            echo
 
-            select opt in "${options[@]}"
-            do
-                case $opt in
-                    "Wyświetl wszystkie informacje")
-                        get_system_info
-                        get_installed_packages
-                        get_running_services
-                        check_open_ports
-                        check_startup_programs
-                        find_recent_packages
-                        check_repositories
-                        if [ "$TRACK_CHANGES" = true ]; then
-                            check_config_changes
-                            track_user_changes
-                            track_binary_changes
-                            track_scheduled_tasks
-                            check_network_changes
-                            check_system_timestamps
-                        fi
-                        break
-                        ;;
-                    "Informacje o systemie")
-                        get_system_info
-                        break
-                        ;;
-                    "Zainstalowane pakiety")
-                        get_installed_packages
-                        break
-                        ;;
-                    "Uruchomione usługi")
-                        get_running_services
-                        break
-                        ;;
-                    "Otwarte porty")
-                        check_open_ports
-                        break
-                        ;;
-                    "Programy startowe")
-                        check_startup_programs
-                        break
-                        ;;
-                    "Ostatnio zainstalowane pakiety")
-                        find_recent_packages
-                        break
-                        ;;
-                    "Skonfigurowane repozytoria")
-                        check_repositories
-                        break
-                        ;;
-                    "Ustaw datę śledzenia zmian")
-                        set_tracking_date
-                        break
-                        ;;
-                    "Zmiany w plikach konfiguracyjnych")
+            read -r -p "Wybierz opcję (1-21): " choice
+            echo
+
+            case $choice in
+                1)
+                    get_system_info
+                    get_installed_packages
+                    get_running_services
+                    check_open_ports
+                    check_startup_programs
+                    find_recent_packages
+                    check_repositories
+                    if [ "$TRACK_CHANGES" = true ]; then
                         check_config_changes
-                        break
-                        ;;
-                    "Zmiany w użytkownikach/grupach")
                         track_user_changes
-                        break
-                        ;;
-                    "Zmiany w plikach binarnych")
                         track_binary_changes
-                        break
-                        ;;
-                    "Zmiany w zaplanowanych zadaniach")
                         track_scheduled_tasks
-                        break
-                        ;;
-                    "Zmiany w konfiguracji sieci")
                         check_network_changes
-                        break
-                        ;;
-                    "Zmiany w kluczowych plikach systemowych")
                         check_system_timestamps
-                        break
-                        ;;
-                    "Raport zmian miesięcznych (ostatnie 12 miesięcy)")
-                        generate_monthly_reports
-                        break
-                        ;;
-                    "Statystyki miesięczne (ostatnie 12 miesięcy)")
-                        generate_monthly_stats
-                        break
-                        ;;
-                    "Wizualizacja zmian miesięcznych")
-                        visualize_monthly_changes
-                        break
-                        ;;
-                    "Zapisz wszystko do pliku")
-                        save_results
-                        break
-                        ;;
-                    "Pomoc")
-                        display_help
-                        break
-                        ;;
-                    "Wyjście")
-                        exit 0
-                        ;;
-                    *)
-                        echo "Nieprawidłowa opcja $REPLY"
-                        break
-                        ;;
-                esac
-            done
+                    fi
+                    ;;
+                2)
+                    get_system_info
+                    ;;
+                3)
+                    get_installed_packages
+                    ;;
+                4)
+                    get_running_services
+                    ;;
+                5)
+                    check_open_ports
+                    ;;
+                6)
+                    check_startup_programs
+                    ;;
+                7)
+                    find_recent_packages
+                    ;;
+                8)
+                    check_repositories
+                    ;;
+                9)
+                    set_tracking_date
+                    ;;
+                10)
+                    check_config_changes
+                    ;;
+                11)
+                    track_user_changes
+                    ;;
+                12)
+                    track_binary_changes
+                    ;;
+                13)
+                    track_scheduled_tasks
+                    ;;
+                14)
+                    check_network_changes
+                    ;;
+                15)
+                    check_system_timestamps
+                    ;;
+                16)
+                    generate_monthly_reports
+                    ;;
+                17)
+                    generate_monthly_stats
+                    ;;
+                18)
+                    visualize_monthly_changes
+                    ;;
+                19)
+                    save_results
+                    ;;
+                20)
+                    display_help
+                    ;;
+                21)
+                    echo -e "${GREEN}Do widzenia!${RESET}"
+                    exit 0
+                    ;;
+                *)
+                    echo -e "${RED}Nieprawidłowa opcja. Wybierz liczbę od 1 do 21.${RESET}"
+                    ;;
+            esac
 
             echo -e "\n${YELLOW}Naciśnij Enter, aby kontynuować...${RESET}"
             read -r
@@ -1463,4 +1439,4 @@ display_help() {
 
 # Uruchom program
 main "$@"
-# Thu Apr  3 12:44:58 PM CEST 2025
+# Thu Apr  3 12:47:27 PM CEST 2025
